8c8ff461d87d986e66f78a55eeab2c3668182909
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2026-07-14 23:52:24 +0700

    chore: update business address, phone number, and Google Maps embed location

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -303,9 +303,6 @@ export default function Home() {
               <a href={`tel:${tBanner("phoneVal1")}`} className="hover:text-amber-500 transition-colors">
                 {tBanner("phoneVal1")}
               </a>
-              <a href={`tel:${tBanner("phoneVal2")}`} className="hover:text-amber-500 transition-colors">
-                {tBanner("phoneVal2")}
-              </a>
             </div>
           </div>
 
@@ -775,7 +772,7 @@ export default function Home() {
       {/* Interactive Google Map Section */}
       <section className="relative w-full h-[350px] md:h-[450px] z-10 -mt-20 md:-mt-24 overflow-hidden border-b border-zinc-100 dark:border-zinc-900">
         <iframe
-          src="https://maps.google.com/maps?q=123%20Nguyen%20Trai%20Street%20District%201%20Ho%20Chi%20Minh%20City&t=&z=16&ie=UTF8&iwloc=&output=embed"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.5949002175816!2d106.7288732753781!3d10.803916398771715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527d83ba221e5%3A%2Fx7348995971a3eca7!2sT99%20Barbershop!5e0!3m2!1sen!2s!4v1752576567530!5m2!1sen!2s"
           width="100%"
           height="100%"
           style={{ border: 0 }}
